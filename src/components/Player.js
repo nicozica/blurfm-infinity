28d4430849118b036387f2e14615ba6946f138cd
Nicolás Zicarelli <nico@argensonix.com.ar>
2024-03-01 11:45:52 -0300

    Album cover function

--- src/components/Player.js
+++ src/components/Player.js
@@ -28,9 +28,9 @@ function formatTitle(title) {
   return title;
 }
 
-
 const Player = () => {
   const [songInfo, setSongInfo] = useState({ artist: '', title: '' });
+  const [coverUrl, setCoverUrl] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -52,19 +52,58 @@ const Player = () => {
 
           // Actualizar el estado con el artista y el título de la canción
           setSongInfo({ artist, title });
+          // Llamar a la función para obtener la portada del álbum
+          getCover(artist, title);
         } else {
           console.error('Error: Source data not found in response');
         }
       } catch (error) {
         console.error('Error fetching data:', error);
+
       }
     };
+    
+    fetchData(); // Llamar a fetchData una vez al montar el componente
 
     const intervalId = setInterval(fetchData, 10000); // Realizar la solicitud cada 10 segundos
 
     // Limpiar el intervalo cuando el componente se desmonte
     return () => clearInterval(intervalId);
   }, []); // Ejecutar solo una vez al montar el componente
+
+
+  // Función para obtener la portada del álbum
+  const getCover = async (artist, title) => {
+    try {
+      // Formatear el artista y el título
+      artist = formatArtist(artist);
+      title = formatTitle(title);
+
+      // Codificar artist y title para la URL
+      artist = encodeURIComponent(artist);
+      title = encodeURIComponent(title);
+
+      // Construir la URL de la solicitud a iTunes
+      const url = `https://itunes.apple.com/search?term=${artist}-${title}&media=music&limit=1`;
+
+      // Realizar la solicitud a iTunes
+      const response = await fetch(url);
+      const data = await response.json();
+
+      // Verificar si se encontró una portada del álbum
+      if (data.results.length === 1) {
+        let cover = data.results[0].artworkUrl100;
+        cover = cover.replace('100x100', '600x600');
+        setCoverUrl(cover);
+      } else {
+        // Si no se encuentra una portada, puedes establecer una imagen predeterminada
+        // o dejar coverUrl vacío para no mostrar ninguna imagen
+        setCoverUrl('../assets/images/logo.png');
+      }
+    } catch (error) {
+      console.error('Error fetching album cover:', error);
+    }
+  };
 
 
 
@@ -82,6 +121,7 @@ const Player = () => {
             />
             <h3>{songInfo.title}</h3>
             <p>{songInfo.artist}</p>
+            {coverUrl && <img src={coverUrl} alt="Album Cover" />}
           </div>
         </div>
       </div>
